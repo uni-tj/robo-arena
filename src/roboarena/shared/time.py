571c72b5e9c6f00f5dcd91d1293aa281e2496ef5
p-98 <34243013+p-98@users.noreply.github.com>
2024-09-04 17:55:10 +0200

    docs: comment shared/time.py

--- src/roboarena/shared/time.py
+++ src/roboarena/shared/time.py
@@ -8,6 +8,7 @@ if TYPE_CHECKING:
 
 
 def get_time() -> "Time":
+    """Abstract over the time module used."""
     return perf_counter()
 
 
@@ -17,8 +18,16 @@ def add_seconds(time: "Time", add: "Time") -> "Time":
 
 @define
 class PreciseClock:
+    """
+    A clock significantly more precise than pygame.Clock.
+
+    Utilizes busy waiting.
+    """
+
     min_dt: "Time"
+    """The minimum frame length"""
     last_t: "Time" = field(factory=get_time, init=False)
+    """The time the last frame started"""
 
     def tick(self) -> float:
         while get_time() - self.last_t < self.min_dt:
