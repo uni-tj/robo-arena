62c05385ff659169a7bf024facff322902429c51
p-98 <34243013+p-98@users.noreply.github.com>
2024-09-04 17:06:43 +0200

    refactor: cleanup shared/game.py

--- src/roboarena/shared/game.py
+++ src/roboarena/shared/game.py
@@ -4,7 +4,6 @@ from collections.abc import Iterable
 from functools import cached_property
 from typing import TYPE_CHECKING, Literal, overload
 
-from roboarena.shared.block import wall
 from roboarena.shared.entity import Entity
 from roboarena.shared.game_ui import GameUI
 from roboarena.shared.types import EntityId, QuitEvent, StartFrameEvent
@@ -15,7 +14,6 @@ from roboarena.shared.util import (
     rect_space_at,
 )
 from roboarena.shared.utils.rect import Rect
-from roboarena.shared.utils.vector import Vector
 
 if TYPE_CHECKING:
     from roboarena.server.level_generation.level_generator import BlockPosition, Level
@@ -67,21 +65,6 @@ class GameState(ABC):
             for _ in entities
             if _.collision.hitbox.overlaps(rect) and _ is not collider
         )
-
-    @overload
-    def collidingWalls(self, collider: Entity) -> bool: ...
-
-    @overload
-    def collidingWalls(self, collider: Rect) -> bool: ...
-
-    def collidingWalls(self, collider: Rect | Entity) -> bool:
-        rect = collider.collision.hitbox if isinstance(collider, Entity) else collider
-        for x in range(rect.top_left.floor().x, rect.bottom_right.ceil().x):
-            for y in range(rect.top_left.floor().y, rect.bottom_right.ceil().y):
-                xy = Vector(x, y)
-                if xy in self.level and self.level[xy] is wall:
-                    return True
-        return False
 
     @frame_cache_method
     def colliding_blocks(
